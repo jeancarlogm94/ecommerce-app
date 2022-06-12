c7eb8e5f46ec027c859da4ffedc042e865c1d422
John Doe <johndoe@example.com>
2022-06-12 10:33:36 -0500

    change styles profile

--- src/pages/Purchases.js
+++ src/pages/Purchases.js
@@ -27,31 +27,36 @@ const Purchases = () => {
   return (
     <Row>
       <Col>
-        <Card.Body style={{ maxWidth: "400px" }} className="mt-5 mx-auto">
-          <Card.Title className="mb-3">My Purchases</Card.Title>
-          {purchases.map((purchase) => (
-            <ListGroup key={purchase.id} style={{ cursor: "pointer" }}>
-              {purchase.cart.products.map((purchaseCart) => (
-                <ListGroupItem
-                  key={purchaseCart.id}
-                  onClick={() => navigate(`/products/${purchaseCart.id}`)}
-                >
-                  {/* <ListGroupItem>{purchaseCart.title}</ListGroupItem> */}
-                  <Alert>
-                    <Card.Title>{purchaseCart.title}</Card.Title>
-                  </Alert>
-                  <ListGroupItem>
-                    <DatePurchase purchase={purchase}></DatePurchase>
+        <Card>
+          <Card.Body
+            style={{ textAlign: "center", maxWidth: "400px" }}
+            className="mt-5 mx-auto"
+          >
+            <Card.Title className="mb-3">My Purchases</Card.Title>
+            {purchases.map((purchase) => (
+              <ListGroup key={purchase.id} style={{ cursor: "pointer" }}>
+                {purchase.cart.products.map((purchaseCart) => (
+                  <ListGroupItem
+                    key={purchaseCart.id}
+                    onClick={() => navigate(`/products/${purchaseCart.id}`)}
+                  >
+                    {/* <ListGroupItem>{purchaseCart.title}</ListGroupItem> */}
+                    <Alert>
+                      <Card.Title>{purchaseCart.title}</Card.Title>
+                    </Alert>
+                    <ListGroupItem>
+                      <DatePurchase purchase={purchase}></DatePurchase>
+                    </ListGroupItem>
+                    <ListGroupItem>Price {purchaseCart.price}</ListGroupItem>
+                    <ListGroupItem>
+                      Quantity {purchaseCart.productsInCart.quantity}
+                    </ListGroupItem>
                   </ListGroupItem>
-                  <ListGroupItem>Price {purchaseCart.price}</ListGroupItem>
-                  <ListGroupItem>
-                    Quantity {purchaseCart.productsInCart.quantity}
-                  </ListGroupItem>
-                </ListGroupItem>
-              ))}
-            </ListGroup>
-          ))}
-        </Card.Body>
+                ))}
+              </ListGroup>
+            ))}
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
   );
